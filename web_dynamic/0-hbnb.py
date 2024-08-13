193d5d2ea8aedda8cd2ab0c3677181ec4a314562
collins-kimotho <collinskimotho16@gmail.com>
2024-08-13 07:18:26 +0300

    airbnb

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -35,7 +35,7 @@ def hbnb():
     places = sorted(places, key=lambda k: k.name)
 
     return render_template('0-hbnb.html',
-                           cache_id=uuid.uuid4()
+                           cache_id=uuid.uuid4(),
                            states=st_ct,
                            amenities=amenities,
                            places=places)
